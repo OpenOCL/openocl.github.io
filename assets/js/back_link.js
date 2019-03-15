8c4d7525ae0f10ee9c041b7ef272c44ba23c4e24
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-03-15 15:30:43 +0100

    Update back_link.js

--- assets/js/back_link.js
+++ assets/js/back_link.js
@@ -2,7 +2,7 @@
 var domain_name = 'openocl';
 var element = document.getElementById('top');
 var last_page = document.referrer;
-if (element !== null && window.history.length > 0 && last_page.toLowerCase().indexOf(domain_name) !== -1)  
+if (element !== null && last_page.toLowerCase().indexOf(domain_name) !== -1)  
 {
   element.setAttribute('href', 'back');
   element.onclick = function() {
