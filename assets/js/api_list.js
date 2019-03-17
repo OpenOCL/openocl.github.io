6fecd7e6006c5243cc6fe9b05cb1a7ff2eed862c
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-03-17 09:12:18 +0100

    Update api_list.js

--- assets/js/api_list.js
+++ assets/js/api_list.js
@@ -1,15 +1,12 @@
 function callback() {
-  if (document.documentElement.clientHeight < 960) {
-    return;
-  }
   let elem = document.getElementById("api-list");
   let coords = elem.getBoundingClientRect();
+  let screen_width = document.documentElement.clientWidth;
   if (coords.top+window.pageYOffset < 50) {
     elem.style.position = "relative";
-  } else if (coords.bottom < document.documentElement.clientHeight-20) {
+  } else if (screen_width>960 && coords.bottom<document.documentElement.clientHeight-20) {
     elem.style.position = "fixed";
     elem.style.bottom = "20px";
   }
 }
-
 window.addEventListener("scroll", callback);
