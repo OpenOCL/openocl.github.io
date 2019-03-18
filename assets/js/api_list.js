04cef2f969b3ab2b22e800b9a337fbf15e37c5ad
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-03-18 12:21:55 +0100

    Update api_list.js

--- assets/js/api_list.js
+++ assets/js/api_list.js
@@ -2,7 +2,7 @@ function callback() {
   let elem = document.getElementById("api-list");
   let coords = elem.getBoundingClientRect();
   let screen_width = document.documentElement.clientWidth;
-  if (coords.top+window.pageYOffset < 50) {
+  if (coords.top+window.pageYOffset < 100) {
     elem.style.position = "relative";
   } else if (screen_width>960 && coords.bottom<document.documentElement.clientHeight-20) {
     elem.style.position = "fixed";
